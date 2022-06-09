d241f1c49a58e3dc1be06ca5af48b975c4cd3b85
skaddie <87253680+skaddie@users.noreply.github.com>
2022-06-10 00:57:08 +0800

    Update sidebar.blade.php

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,8 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('home')}}" class="brand-link">
-        <img src="https://cdn-icons-png.flaticon.com/512/2258/2258439.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-             style="opacity: .8">
+        <i class="fa-solid fa-cash-register"  style="opacity: .8; color:white; font-size: 0.9rem;"></i>
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
 
@@ -12,7 +11,7 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="https://cdn-icons.flaticon.com/png/128/2202/premium/2202112.png?token=exp=1654790653~hmac=0f03ab3f77fcccdd97aebc127bb76b71" class="img-circle elevation-2" alt="User Image">
+            <i class="fa-solid fa-user" style="color:white; font-size: 0.9rem;"></i>
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ auth()->user()->getFullname() }}</a>
